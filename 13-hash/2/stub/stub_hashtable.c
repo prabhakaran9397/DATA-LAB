53fc464fa71e7fc30fb65bb598a6e1aecc240671
prabhakaran9397 <prabhakaran9397@gmail.com>
2016-02-13 00:38:34 +0530

    13/2 done no readme

--- 13-hash/2/stub/stub_hashtable.c
+++ 13-hash/2/stub/stub_hashtable.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
-#include "hashtable.h"
+#include <stdlib.h>
+#include <string.h>
+
+struct entry
+{
+	int key;
+	int val;
+};
+struct HashTable
+{
+	int size;
+	struct entry *arr;
+};
+
+void create(struct HashTable *h, int siz);
+int insert(struct HashTable *h, struct entry *e);
+int search(struct HashTable *h, int key, int *val, int *nops);
 
 int main()
 {
-	HashTable ht(10);
+	struct HashTable *ht;
+	ht = (struct HashTable *)malloc(sizeof(struct HashTable));
+	create(ht,10);
 	struct entry arr[] = {
 		{ 1, 12 }, { 2, 14 }, { 3, 39 }, { 4, 47 }, { 5, 8 },
 		{ 6, 23 }, { 7, 33 }, { 8, 19 }, { 9, 76 }, { 10, 20 }
@@ -12,21 +30,21 @@ int main()
 
 	printf("testing insert...");
 	for (i = 0; i < 10; i++) {
-		if (!ht.insert(&arr[i]))
+		if (!insert(ht,&arr[i]))
 			goto fail;
 	}
-	if (ht.insert(&arr[0]))
+	if (insert(ht,&arr[0]))
 		goto fail;
 	printf("[ok]\n");
 
 	printf("testing search...");
 	for (i = 0; i < 10; i++) {
-		if (!ht.search(arr[i].key, &val, &nops))
+		if (!search(ht,arr[i].key, &val, &nops))
 			goto fail;
 		if (val != arr[i].val || nops != expected_nops[i])
 			goto fail;
 	}
-	if (ht.search(99, &val, &nops))
+	if (search(ht,99, &val, &nops))
 		goto fail;
 	printf("[ok]\n");
 
@@ -35,4 +53,6 @@ int main()
 fail:
 	printf("[failed]\n");
 	return -1;
+	
 }
+
